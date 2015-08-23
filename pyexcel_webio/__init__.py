6278276bc1209ffb15a8e06f5a30ba989ea21a9c
chfw <wangc_2011@hotmail.com>
2015-08-23 01:43:46 +0100

    fix pep8 warnings and update documentation

--- pyexcel_webio/__init__.py
+++ pyexcel_webio/__init__.py
@@ -40,10 +40,10 @@ class ExcelInput(object):
     def load_single_sheet(self, sheet_name=None, **keywords):
         """Abstract method
 
-        :param form_field_name: the file field name in the html form for file upload
-        :param sheet_name: For an excel book, there could be multiple sheets. If it is left
-                         unspecified, the sheet at index 0 is loaded. For 'csv', 'tsv' file,
-                         *sheet_name* should be None anyway.
+        :param sheet_name: For an excel book, there could be multiple
+                           sheets. If it is left unspecified, the
+                           sheet at index 0 is loaded. For 'csv', 'tsv'
+                           file, *sheet_name* should be None anyway.
         :param keywords: additional key words
         :returns: A sheet object
         """
@@ -51,8 +51,9 @@ class ExcelInput(object):
 
     def load_book(self, **keywords):
         """Abstract method
-        
-        :param form_field_name: the file field name in the html form for file upload
+
+        :param form_field_name: the file field name in the html
+                                form for file upload
         :param keywords: additional key words
         :returns: A instance of :class:`Book`
         """
@@ -61,24 +62,24 @@ class ExcelInput(object):
     def get_sheet(self, sheet_name=None, **keywords):
         """
         Get a :class:`Sheet` instance from the file
-        
-        :param form_field_name: the file field name in the html form for file upload
-        :param sheet_name: For an excel book, there could be multiple sheets. If it is left
-                         unspecified, the sheet at index 0 is loaded. For 'csv', 'tsv' file,
-                         *sheet_name* should be None anyway.
+
+        :param sheet_name: For an excel book, there could be multiple
+                           sheets. If it is left unspecified, the
+                           sheet at index 0 is loaded. For 'csv',
+                           'tsv' file, *sheet_name* should be None anyway.
         :param keywords: additional key words
         :returns: A sheet object
         """
         return self.load_single_sheet(sheet_name=sheet_name, **keywords)
-        
+
     def get_array(self, sheet_name=None, **keywords):
         """
         Get a list of lists from the file
-        
-        :param form_field_name: the file field name in the html form for file upload
-        :param sheet_name: For an excel book, there could be multiple sheets. If it is left
-                         unspecified, the sheet at index 0 is loaded. For 'csv', 'tsv' file,
-                         *sheet_name* should be None anyway.
+
+        :param sheet_name: For an excel book, there could be multiple
+                           sheets. If it is left unspecified, the
+                           sheet at index 0 is loaded. For 'csv',
+                           'tsv' file, *sheet_name* should be None anyway.
         :param keywords: additional key words
         :returns: A list of lists
         """
@@ -90,15 +91,18 @@ class ExcelInput(object):
 
     def get_dict(self, sheet_name=None, name_columns_by_row=0, **keywords):
         """Get a dictionary from the file
-        
-        :param form_field_name: the file field name in the html form for file upload
-        :param sheet_name: For an excel book, there could be multiple sheets. If it is left
-                         unspecified, the sheet at index 0 is loaded. For 'csv', 'tsv' file,
-                         *sheet_name* should be None anyway.
+
+        :param sheet_name: For an excel book, there could be multiple
+                           sheets. If it is left unspecified, the
+                           sheet at index 0 is loaded. For 'csv',
+                           'tsv' file, *sheet_name* should be None anyway.
         :param keywords: additional key words
         :returns: A dictionary
         """
-        sheet = self.load_single_sheet(sheet_name=sheet_name, name_columns_by_row=name_columns_by_row, **keywords)
+        sheet = self.load_single_sheet(
+            sheet_name=sheet_name,
+            name_columns_by_row=name_columns_by_row,
+            **keywords)
         if sheet:
             return sheet.to_dict()
         else:
@@ -106,15 +110,18 @@ class ExcelInput(object):
 
     def get_records(self, sheet_name=None, name_columns_by_row=0, **keywords):
         """Get a list of records from the file
-  
-        :param form_field_name: the file field name in the html form for file upload
-        :param sheet_name: For an excel book, there could be multiple sheets. If it is left
-                         unspecified, the sheet at index 0 is loaded. For 'csv', 'tsv' file,
-                         *sheet_name* should be None anyway.
+
+        :param sheet_name: For an excel book, there could be multiple
+                           sheets. If it is left unspecified, the
+                           sheet at index 0 is loaded. For 'csv',
+                           'tsv' file, *sheet_name* should be None anyway.
         :param keywords: additional key words
         :returns: A list of records
         """
-        sheet = self.load_single_sheet(sheet_name=sheet_name, name_columns_by_row=name_columns_by_row, **keywords)
+        sheet = self.load_single_sheet(
+            sheet_name=sheet_name,
+            name_columns_by_row=name_columns_by_row,
+            **keywords)
         if sheet:
             return sheet.to_records()
         else:
@@ -142,7 +149,6 @@ class ExcelInput(object):
     def get_book(self, **keywords):
         """Get a instance of :class:`Book` from the file
 
-        :param form_field_name: the file field name in the html form for file upload
         :param keywords: additional key words
         :returns: A instance of :class:`Book`
         """
@@ -151,7 +157,6 @@ class ExcelInput(object):
     def get_book_dict(self, **keywords):
         """Get a dictionary of two dimensional array from the file
 
-        :param form_field_name: the file field name in the html form for file upload
         :param keywords: additional key words
         :returns: A dictionary of two dimensional arrays
         """
@@ -161,10 +166,11 @@ class ExcelInput(object):
         else:
             return None
 
-    def save_book_to_database(self,
-                              session=None, tables=None,
-                              initializers=None, mapdicts=None, auto_commit=True,
-                              **keywords):
+    def save_book_to_database(
+            self,
+            session=None, tables=None,
+            initializers=None, mapdicts=None, auto_commit=True,
+            **keywords):
         book = self.load_book(**keywords)
         if book:
             book.save_to_database(session,
@@ -172,7 +178,6 @@ class ExcelInput(object):
                                   initializers=initializers,
                                   mapdicts=mapdicts,
                                   auto_commit=auto_commit)
-
 
 
 class ExcelInputInMultiDict(ExcelInput):
@@ -193,7 +198,7 @@ class ExcelInputInMultiDict(ExcelInput):
 
     def load_book(self, field_name=None, **keywords):
         file_type, file_handle = self.get_file_tuple(field_name)
-        if file_type is not None and file_handle is not None:        
+        if file_type is not None and file_handle is not None:
             return pe.get_book(file_type=file_type,
                                file_content=file_handle.read(),
                                **keywords)
@@ -209,11 +214,13 @@ ExcelResponse = dummy_func
 
 
 def make_response(pyexcel_instance, file_type, status=200, **keywords):
-    """Make a http response from a pyexcel instance of :class:`~pyexcel.Sheet` or :class:`~pyexcel.Book`
+    """
+    Make a http response from a pyexcel instance of
+    :class:`~pyexcel.Sheet` or :class:`~pyexcel.Book`
 
     :param pyexcel_instance: pyexcel.Sheet or pyexcel.Book
     :param file_type: one of the following strings:
-                      
+
                       * 'csv'
                       * 'tsv'
                       * 'csvz'
@@ -222,41 +229,60 @@ def make_response(pyexcel_instance, file_type, status=200, **keywords):
                       * 'xlsx'
                       * 'xlsm'
                       * 'ods'
-                        
+
     :param status: unless a different status is to be returned.
     """
     io = pe.get_io(file_type)
     pyexcel_instance.save_to_memory(file_type, io, **keywords)
     io.seek(0)
-    return ExcelResponse(io.read(), content_type=FILE_TYPE_MIME_TABLE[file_type], status=status)
+    return ExcelResponse(io.read(),
+                         content_type=FILE_TYPE_MIME_TABLE[file_type],
+                         status=status)
 
 
-def make_response_from_array(array, file_type, status=200, **keywords):
-    return make_response(pe.Sheet(array), file_type, status, **keywords)
-
-    
-def make_response_from_dict(adict, file_type, status=200, **keywords):
-    return make_response(pe.load_from_dict(adict), file_type, status, **keywords)
-
-
-def make_response_from_records(records, file_type, status=200, **keywords):
-    return make_response(pe.load_from_records(records), file_type, status, **keywords)
+def make_response_from_array(array,
+                             file_type, status=200,
+                             **keywords):
+    return make_response(pe.Sheet(array),
+                         file_type, status, **keywords)
 
 
-def make_response_from_book_dict(adict, file_type, status=200, **keywords):
+def make_response_from_dict(adict,
+                            file_type, status=200,
+                            **keywords):
+    return make_response(pe.load_from_dict(adict),
+                         file_type, status, **keywords)
+
+
+def make_response_from_records(records,
+                               file_type, status=200,
+                               **keywords):
+    return make_response(pe.load_from_records(records),
+                         file_type, status, **keywords)
+
+
+def make_response_from_book_dict(adict,
+                                 file_type, status=200,
+                                 **keywords):
     return make_response(pe.Book(adict), file_type, status, **keywords)
 
 
-def make_response_from_query_sets(query_sets, column_names, file_type, status=200, **keywords):
+def make_response_from_query_sets(query_sets, column_names,
+                                  file_type, status=200,
+                                  **keywords):
     sheet = pe.get_sheet(query_sets=query_sets, column_names=column_names)
     return make_response(sheet, file_type, status, **keywords)
 
 
-def make_response_from_a_table(session, table, file_type, status=200, **keywords):
+def make_response_from_a_table(session, table,
+                               file_type, status=200,
+                               **keywords):
     sheet = pe.get_sheet(session=session, table=table, **keywords)
     return make_response(sheet, file_type, status, **keywords)
 
 
-def make_response_from_tables(session, tables, file_type, status=200, **keywords):
+def make_response_from_tables(session, tables,
+                              file_type, status=200,
+                              **keywords):
     book = pe.get_book(session=session, tables=tables, **keywords)
     return make_response(book, file_type, status, **keywords)
