476152238ca5dc8e3ac92a840e37ce0bcf9e7bf3
chfw <wangc_2011@hotmail.com>
2016-01-26 23:27:32 +0000

    remove duplicated information scattered around pyexcel components

--- setup.py
+++ setup.py
@@ -9,41 +9,51 @@ with open("README.rst", 'r') as readme:
     README_txt = readme.read()
 
 dependencies = [
-    'pyexcel>=0.2.0'
+    'pyexcel>=0.2.0',
 ]
 
-with open("VERSION", "r") as version:
-    version_txt = version.read().rstrip()
+extras = {}
+
 
 setup(
     name='pyexcel-webio',
-    author="C. W.",
-    version=version_txt,
-    author_email="wangc_2011@hotmail.com",
-    url="https://github.com/chfw/pyexcel-webio",
+    author='C. W.',
+    version='0.0.6',
+    author_email='wangc_2011 (at) hotmail.com',
+    url='https://github.com/pyexcel/pyexcel-webio',
     description='A generic request and response interface for pyexcel web extensions.',
     install_requires=dependencies,
+    extras_require=extras,
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     long_description=README_txt,
     zip_safe=False,
     tests_require=['nose'],
-    keywords=['API', 'pyexcel', 'Excel', 'HTTP'],
-    license='BSD',
+    keywords=[
+        'excel',
+        'python',
+        'pyexcel',
+        'http'
+    ],
+    license='New BSD',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
         'Programming Language :: Python',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: PyPy'
     ]
 )
