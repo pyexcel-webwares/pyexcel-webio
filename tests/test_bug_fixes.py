b91032a93925da9434d627c6e1317aa4fae86331
chfw <wangc_2011@hotmail.com>
2017-03-02 23:30:23 +0000

    fix broken test

--- tests/test_bug_fixes.py
+++ tests/test_bug_fixes.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 from common import TestExtendedInput
 from nose.tools import eq_
+
+
+PY2 = sys.version_info[0] == 2
 
 
 def test_issue_4():
     myinput = TestExtendedInput()
     fixture = os.path.join("tests", "fixtures", "issue4-broken.csv")
-    with open(fixture, "r") as f:
-        array = myinput.get_array(field_name=('csv', f), encoding='latin1')
-        expected = [
-            [u'Last Name', u'First Name', u'Company', u'Email', u'Job Title'],
-            [u'Test', u'Th\xefs', u'Cool Co',
-             u'test.this@example.com', u'Founder']
-        ]
-        eq_(array, expected)
+
+    expected = [
+        [u'Last Name', u'First Name', u'Company', u'Email', u'Job Title'],
+        [u'Test', u'Th\xefs', u'Cool Co',
+         u'test.this@example.com', u'Founder']
+    ]
+    if PY2:
+        with open(fixture, "rb") as f:
+            array = myinput.get_array(field_name=('csv', f),
+                                      encoding='latin1')
+            eq_(array, expected)
+    else:
+        with open(fixture, "r", encoding='latin1') as f:
+            array = myinput.get_array(field_name=('csv', f))
+            eq_(array, expected)
+
