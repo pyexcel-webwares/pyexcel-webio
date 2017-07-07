2ab7b579cd25a0ff88f3701740c352a924b4d524
chfw <wangc_2011@hotmail.com>
2017-07-07 21:04:01 +0100

    kickstart 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    'pyexcel': ('http://pyexcel.readthedocs.org/en/latest/', None),
+    'pyexcel': ('http://pyexcel.readthedocs.io/en/latest/', None),
 }
 spelling_word_list_filename = 'spelling_wordlist.txt'
 templates_path = ['_templates']
@@ -20,8 +20,8 @@ master_doc = 'index'
 
 project = u'pyexcel-webio'
 copyright = u'2015-2017 Onni Software Ltd.'
-version = '0.0.11'
-release = '0.1.0'
+version = '0.1.0'
+release = '0.1.1'
 exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'default'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 NAME = 'pyexcel-webio'
 AUTHOR = 'C.W.'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'New BSD'
 DESCRIPTION = (
